af9566ba6317f41f06e4177445aa5f63b399d0d8
dovbeta <dovbeta@gmail.com>
2017-07-19 11:11:55 +0300

    Added models for financial stuff (Accounts, Services, Payments etc.)

--- osbb/models.py
+++ osbb/models.py
@@ -62,3 +62,61 @@ class Person(models.Model):
 
     def __str__(self):
         return self.last_name + ' ' + self.first_name + (' (owner)' if self.is_owner else '')
+
+
+class PersonalAccount(models.Model):
+    apartment = models.OneToOneField(Apartment, on_delete=models.CASCADE, primary_key=True)
+    number = models.CharField(max_length=20)
+
+
+class Balance(models.Model):
+    personal_account = models.ForeignKey(PersonalAccount, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True)
+    value = models.FloatField()
+
+
+class OrgAccount(models.Model):
+    title = models.CharField(max_length=20)
+    number = models.CharField(max_length=20)
+
+
+class OrgBalance(models.Model):
+    org_account = models.ForeignKey(OrgAccount, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True)
+    value = models.FloatField()
+
+
+class Payment(models.Model):
+    personal_account = models.ForeignKey(PersonalAccount, on_delete=models.CASCADE)
+    org_account = models.ForeignKey(OrgAccount, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True)
+    value = models.FloatField()
+
+
+class OrgCost(models.Model):
+    org_account = models.ForeignKey(OrgAccount, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True)
+    value = models.FloatField()
+    receiver = models.CharField(max_length=100)
+    comment = models.CharField(max_length=200)
+
+
+class Service(models.Model):
+    personal_accounts = models.ManyToManyField(PersonalAccount)
+    title = models.CharField(max_length=200)
+    tariff = models.FloatField(default=0)
+    formula = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Accrual(models.Model):
+    personal_account = models.ForeignKey(PersonalAccount, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True)
+
+
+class AccrualDetail(models.Model):
+    accrual = models.ForeignKey(Accrual, on_delete=models.CASCADE)
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+    value = models.FloatField()
+    date = models.DateTimeField(auto_now=True)
